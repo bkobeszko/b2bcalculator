3ffd5b33176974831523ad290cf07762ec1437b9
Bartłomiej Kobeszko <Bartomiej Kobeszko>
2019-01-20 15:16:47 +0100

    version and update date in GUI

--- src/main/java/pl/bkobeszko/b2bcalculator/MainController.java
+++ src/main/java/pl/bkobeszko/b2bcalculator/MainController.java
@@ -2,6 +2,7 @@ package pl.bkobeszko.b2bcalculator;
 
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,22 @@ public class MainController {
     
     @Autowired
     private TaxFactorsStoreFactory taxFactorsStoreFactory;
+    
+    @Value("${app.version}")
+    private String applicationVersion;
+    
+    @Value("${app.taxUpdateDate}")
+    private String taxUpdateDate;
+    
+    @ModelAttribute("applicationVersion")
+    public String getApplicationVersion() {
+        return applicationVersion;
+    }
+    
+    @ModelAttribute("taxUpdateDate")
+    public String getTaxUpdateDate() {
+        return taxUpdateDate;
+    }
     
     @ModelAttribute("years")
     public List<Integer> years() {
